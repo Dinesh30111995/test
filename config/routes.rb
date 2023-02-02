012cd0e06caebad44ac640deb016aa1e6e95a590
Sam Wagner <60307610+ideadesignmedia@users.noreply.github.com>
2023-02-02 11:48:41 -0700

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -130,7 +130,7 @@ Rails.application.routes.draw do
 
   ### STATIC PAGES
   get 'about', to: 'static#about'
-  get 'dt6', to: 'static#dt6'
+  get 'airdrop', to: 'static#airdrop'
   get 'faq', to: 'static#faq'
   get 'terms_conditions', to: 'static#terms_conditions'
   get 'privacy', to: 'static#privacypolicy'
